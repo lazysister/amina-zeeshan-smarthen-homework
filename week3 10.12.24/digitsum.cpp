a1b1ed359557289592903998b378911cd589da44
lazysister <amnazeeshan1000@gmail.com>
2025-01-31 18:17:30 +0500

    Add files via upload

---
+++ week3 10.12.24/digitsum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=0;
+    int sum;
+    
+    while ( i>= 0)
+     
+    {sum=sum+i;
+        cout<<"enter a number: ";
+        cin>>i;              
+    } 
+    cout<<"sum: "<<sum;
+
+    
+    
+    return 0;}
---
+++ week3 10.12.24/happy.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char i;
+  
+    while (( i!='n') && (i!='N') )
+    {
+        cout<<"i am happy ! "<<endl;
+        cout<<"enter your choice: ";
+              cin>>i;    
+    }
+    
+    
+    return 0;
+}
